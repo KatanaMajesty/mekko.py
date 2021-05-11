fd018a66ccc584ff8f02c16f9200bae40806e420
l_luk <78260779+LuK050@users.noreply.github.com>
2021-05-11 21:19:32 +0300

    Add files via upload

--- cogs/serversinfo.py
+++ cogs/serversinfo.py
@@ -22,27 +22,20 @@ class User(commands.Cog):
 
         url2 = f'https://minecraft-statistic.net/api/server/info/138.201.182.132_25565/'
         response = requests.get(url2)
-    	list = response.json()["info"]["players_list"]
+        list = response.json()["info"]["players_list"]
 
-    	online = response.json()["info"]["players"]
-    	list_count = int(online) - int(len(list)) 
-
-
-    	if online == 12:
-	    count = '.'
-    	elif online < 12:
-	    count = '.'
-    	elif online > 12:
-	    count = f' и ещё {list_count} игроков'
+        online = response.json()["info"]["players"]
+        list_count = int(online) - int(len(list)) 
+        
 
 
+        ms = minestat.MineStat('138.201.182.132', 25565)
+        urv = ctx.guild.icon_url
+        embed = discord.Embed(title='Информация о сервере',description=f'**• Дискорд сервер**\nНазвание **{ctx.guild.name}**\nСоздан {ctx.guild.created_at.strftime("**%Y.%m.%d** в **%H:%M**")}\nВсего **{ctx.guild.member_count}** участников\n\n**• Игровой сервер**\nСтатус: **Закрыт навсегда**',colour=discord.Colour.blurple())
+        embed.set_thumbnail(url=urv)
+        await ctx.send(embed=embed)
+        return
 
-    	ms = minestat.MineStat('138.201.182.132', 25565)
-    	urv = ctx.guild.icon_url
-    	embed = discord.Embed(title='Информация о сервере',description=f'**• Дискорд сервер**\nНазвание **{ctx.guild.name}**\nСоздан {ctx.guild.created_at.strftime("**%Y.%m.%d** в **%H:%M**")}\nВсего **{ctx.guild.member_count}** участников\n\n**• Игровой сервер**\nСтатус: **Закрыт**',colour=discord.Colour.blurple())
-    	embed.set_thumbnail(url=urv)
-    	await ctx.send(embed=embed)
-    	return
 
 
 
